5062fa3032042eff295661259b0f867a420163ac
RedDotz20 <carlos_tabangay@outlook.com>
2023-02-10 21:39:15 +0800

    build: main app server changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,76 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
-import { urlencoded } from "body-parser";
+import bodyParser, { urlencoded } from "body-parser";
 import { notFound, errorHandler } from "./middleware/index";
-import { getUsers } from "./routes/users";
+import logging from "./config/logging";
+import path from "path";
+import createError from "http-errors";
 
-const app = express();
+// import { check, validationResult } from "express-validator";
+// import jwt from "jsonwebtoken";
+// import connection from "./connection";
+// import bcrypt from "bcryptjs";
+// import { getUsers } from "./routes/users";
 
-app.use(cors());
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
+const NAMESPACE = "Server";
+// const app = express();
+const router = express();
+
+router.use(cors());
+router.use(express.json());
+
+//* Parse the body of the request
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+//* Log the request
+router.use((req, res, next) => {
+	/** Log the req */
+	logging.info(
+		NAMESPACE,
+		`METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+	);
+
+	res.on("finish", () => {
+		/** Log the res */
+		logging.info(
+			NAMESPACE,
+			`METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`
+		);
+	});
+});
+
+//* Rules of API
+router.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+
+	if (req.method == "OPTIONS") {
+		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+		return res.status(200).json({});
+	}
+
+	next();
+});
 
 //* Routes
-
-app.use(getUsers);
-
-app.get("/", (req, res) => {
+router.get("/test", (req: Request, res: Response) => {
 	res.send("Hello world");
 });
 
 //! Error Handling
-app.use(notFound);
-app.use(errorHandler);
+router.use((req, res, next) => {
+	const error = new Error("Not found");
 
-export default app;
+	res.status(404).json({
+		message: error.message
+	});
+});
+
+router.use(notFound);
+router.use(errorHandler);
+
+export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,23 @@
-import app from "./app";
+import http from "http";
+import router from "./app";
 import dotenv from "dotenv";
+import config from "./config/config";
+import logging from "./config/logging";
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log(`Server Connected Successfully on http://localhost:${PORT}`);
-});
+// router.listen(PORT, () => {
+// 	console.log(`Server Connected Successfully on http://localhost:${PORT}`);
+// });
+
+const NAMESPACE = "Server";
+
+const httpServer = http.createServer(router);
+
+httpServer.listen(config.server.port, () =>
+	logging.info(
+		NAMESPACE,
+		`Server is running ${config.server.hostname}:${config.server.port}`
+	)
+);
