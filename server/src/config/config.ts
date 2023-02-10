fc7719e455d6c180bb445c177fdbdcc9442a53e6
RedDotz20 <carlos_tabangay@outlook.com>
2023-02-10 21:36:49 +0800

    chore: config changes

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
+const MYSQL_HOST = process.env.MYSQL_HOST || "localhost";
+const MYSQL_USER = process.env.MYSQL_USER || "root";
+const MYSQL_PASS = process.env.MYSQL_PASS || "admin";
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || "sad24_ims_system";
+
+const MYSQL = {
+	host: MYSQL_HOST,
+	user: MYSQL_USER,
+	password: MYSQL_PASS,
+	database: MYSQL_DATABASE
+};
+
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
-const SERVER_PORT = process.env.SERVER_PORT || 3500;
+const SERVER_PORT = process.env.SERVER_PORT || 4000;
 const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_PORT || 3600;
 const SERVER_TOKEN_ISSUER = process.env.ISSUER || "coolIssuer";
 const SERVER_TOKEN_SECRET = process.env.SECRET || "superencryptedsecret";
@@ -18,6 +31,7 @@ const SERVER = {
 };
 
 const config = {
+	mysql: MYSQL,
 	server: SERVER
 };
 
