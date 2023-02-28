6b7a7b87a9f08ab950aa07788bf47bfc4a23a5b0
RedDotz20 <carlos_tabangay@outlook.com>
2023-02-28 23:33:31 +0800

    fix: random route redirect

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,15 +1,17 @@
 import { useNavigate } from "react-router-dom";
-import { Button } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 function NotFound() {
 	const navigate = useNavigate(),
 		redirectToLogin = () => navigate("/login", { replace: true });
+
+	useEffect(() => {
+		setTimeout(() => redirectToLogin(), 2000);
+	}, []);
+
 	return (
-		<div className="bg-[#333] h-screen flex flex-col items-center justify-center">
+		<div className="bg-[#333] h-screen w-screen flex flex-col items-center justify-center">
 			<h1 className="text-white text-4xl font-bold mb-4">404 Not Found</h1>
-			<Button onClick={redirectToLogin} colorScheme="orange" border="1px">
-				Return To Login
-			</Button>
 		</div>
 	);
 }
--- client/src/routes/SidebarRoutes.tsx
+++ client/src/routes/SidebarRoutes.tsx
@@ -5,11 +5,13 @@ const Home = lazy(() => import("../pages/Home"));
 const Dashboard = lazy(() => import("../pages/Dashboard"));
 const Products = lazy(() => import("../pages/Products"));
 const Sales = lazy(() => import("../pages/Sales"));
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 export default function SidebarRoutes() {
 	return (
 		<Suspense fallback={<Loader />}>
 			<Routes>
+				<Route path="*" element={<NotFound />} />
 				<Route path="home" element={<Home />} />
 				<Route path="dashboard" element={<Dashboard />} />
 				<Route path="products" element={<Products />} />
