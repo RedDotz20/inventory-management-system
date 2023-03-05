5b13ff4ce44b58f912e03d89d888bde2887b3d05
RedDotz20 <carlos_tabangay@outlook.com>
2023-03-05 20:27:03 +0800

    feat: product add button

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,3 +1,5 @@
+import { Stack, Button } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import SearchBox from "../components/searchBox/SearchBox";
 
 export default function Products() {
@@ -5,8 +7,17 @@ export default function Products() {
 		<div className="w-full flex p-8">
 			<div className="flex flex-col w-full">
 				<h1 className="text-3xl mb-8">Products Page</h1>
-				<div className="">
+				<div className="flex">
 					<SearchBox />
+					<Stack direction="row">
+						<Button
+							leftIcon={<AddIcon />}
+							className="mt-auto mb-4 mx-4"
+							colorScheme="orange"
+						>
+							Add
+						</Button>
+					</Stack>
 				</div>
 			</div>
 		</div>
