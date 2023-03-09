42c0d809b5f26a554e128838a6bf3dcee9c044f7
RedDotz20 <carlos_tabangay@outlook.com>
2023-03-09 17:12:23 +0800

    feat: products table component

---
+++ client/src/components/tables/ProductTable.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductTable() {
+	return <div>ProductTable</div>;
+}
--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,18 +1,31 @@
-import { Stack, Button } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import {
+	Stack,
+	Button,
+	Table,
+	Thead,
+	Tbody,
+	Tfoot,
+	Tr,
+	Th,
+	Td,
+	TableCaption,
+	TableContainer,
+} from "@chakra-ui/react";
 import SearchBox from "../components/searchBox/SearchBox";
 
 export default function Products() {
 	return (
-		<div className="w-full flex p-8">
-			<div className="flex flex-col w-full">
-				<h1 className="text-3xl mb-8">Products Page</h1>
+		<div className="w-full flex p-8 flex-col">
+			<h1 className="text-3xl mb-8">Products Page</h1>
+			<div className="flex flex-col">
 				<div className="flex">
 					<SearchBox
 						borderColor="black"
 						_hover={{ borderColor: "black" }}
 						focusBorderColor="tranparent"
 					/>
+
 					<Stack direction="row">
 						<Button
 							leftIcon={<AddIcon />}
@@ -23,6 +36,23 @@ export default function Products() {
 						</Button>
 					</Stack>
 				</div>
+
+				<TableContainer>
+					<Table variant="simple" size="sm">
+						<Thead>
+							<Tr>
+								<Th>ID</Th>
+								<Th>NAME</Th>
+								<Th>CATEGORY</Th>
+								<Th>UNIT</Th>
+								<Th>BRAND</Th>
+								<Th>PRICE</Th>
+								<Th>ACTIONS</Th>
+							</Tr>
+						</Thead>
+						<Tbody></Tbody>
+					</Table>
+				</TableContainer>
 			</div>
 		</div>
 	);
