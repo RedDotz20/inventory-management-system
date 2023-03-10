f17622fd3fb1ffbad68301e56eea9414386bb21a
RedDotz20 <carlos_tabangay@outlook.com>
2023-03-10 11:50:53 +0800

    feat: helmet.js add

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import helmet from "helmet";
 
-import loginAuth from "./routes/userAuthRoute";
+import userAuthRoute from "./routes/userAuthRoute";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(loginAuth);
+app.use(userAuthRoute);
 
 export default app;
