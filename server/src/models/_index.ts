ddde7d7f2d407d31fc5abf992aae60c170dc805f
RedDotz20 <carlos_tabangay@outlook.com>
2023-03-15 15:27:52 +0800

    feat: createTable add

--- server/src/models/_index.ts
+++ server/src/models/_index.ts
@@ -1,8 +1,28 @@
-async function createTables(): Promise<void> {
-	// const conn = await createConnection();
-	// const usersSchema = fs.readFileSync("users.sql", "utf8");
-	// const postsSchema = fs.readFileSync("posts.sql", "utf8");
-	// await conn.execute(usersSchema);
-	// await conn.execute(postsSchema);
-	// conn.release();
-}
+import connection from "../config/connection";
+import path from "path";
+import fs from "fs";
+
+const createTables = async (): Promise<void> => {
+	try {
+		const createModel = (schema: string) => {
+			const filepath = path.join(__dirname, schema);
+			return fs.readFileSync(filepath, "utf8");
+		};
+
+		const users = createModel("users.sql");
+		const productUnit = createModel("productUnit.sql");
+		const productCategory = createModel("productCategory.sql");
+		const products = createModel("products.sql");
+
+		await connection.execute(users);
+		await connection.execute(productUnit);
+		await connection.execute(productCategory);
+		await connection.execute(products);
+
+		console.log(`Tables Sucessfully Created`);
+	} catch (err) {
+		console.error(`Error executing SQL file:`, err);
+	}
+};
+
+export default createTables;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app";
 import config from "./config/config";
 import logging from "./config/logging";
+import createTables from "./models/_index";
 
-app.listen(config.server.port, () =>
+app.listen(config.server.port, async () => {
+	await createTables();
 	logging.info(
 		"Server",
 		`Running on http://${config.server.hostname}:${config.server.port}`
-	)
-);
+	);
+});
