d17c99a3238353ba7a51eec6bf46723050a37f64
RedDotz20 <carlos_tabangay@outlook.com>
2023-03-16 21:38:13 +0800

    chore: react-query v4 install

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,23 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./utils/ErrorBoundary";
-import ErrorFallback from "./utils/ErrorFallback";
 import theme from "./theme/Theme";
 
 import "typeface-roboto";
 import "./styles/index.css";
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			refetchOnMount: false,
+			staleTime: Infinity,
+		},
+	},
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ErrorBoundary>
-			<ChakraProvider theme={theme}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</ChakraProvider>
+			<QueryClientProvider client={queryClient}>
+				<ChakraProvider theme={theme}>
+					<BrowserRouter>
+						<App />
+						<ReactQueryDevtools initialIsOpen={true} />
+					</BrowserRouter>
+				</ChakraProvider>
+			</QueryClientProvider>
 		</ErrorBoundary>
 	</React.StrictMode>
 );
