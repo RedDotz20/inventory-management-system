d79c1fea8d3918502ea63469368f1c27e7caa8c2
RedDotz20 <carlos_tabangay@outlook.com>
2023-04-02 11:18:51 +0800

    styles: delete prod modal

--- apps/client/src/pages/Products/components/DeleteProduct/DeleteProductModal.tsx
+++ apps/client/src/pages/Products/components/DeleteProduct/DeleteProductModal.tsx
@@ -1,15 +1,46 @@
+import { motion } from 'framer-motion';
+import { Heading, Button, Flex } from '@chakra-ui/react';
+import { IoCloseSharp } from 'react-icons/io5';
 import Modal from '../../../../components/Modal';
 import Backdrop from '../../../../components/Backdrop';
 
 type ModalProps = { closeModal: () => void; isOpen?: boolean };
 
 function DeleteProductContent({ closeModal }: ModalProps) {
+  const MotionButton = motion(Button);
   return (
     <>
-      <h1 className="">This is the DELETE product Modal</h1>
-      <button onClick={closeModal} className="">
-        CLOSE MODAL
-      </button>
+      <IoCloseSharp
+        className="absolute top-4 right-4 cursor-pointer"
+        color="red"
+        size={24}
+        onClick={closeModal}
+      />
+
+      <Heading as="h3" size="md" mt="1.5rem">
+        Are you sure you want to delete
+      </Heading>
+
+      <Flex gap="2" width="100%" mt="auto">
+        <MotionButton
+          width="100%"
+          variant="outline"
+          onClick={closeModal}
+          colorScheme="orange"
+          whileTap={{ scale: 0.9 }}
+        >
+          CANCEL
+        </MotionButton>
+
+        <MotionButton
+          width="100%"
+          onClick={closeModal}
+          colorScheme="red"
+          whileTap={{ scale: 0.9 }}
+        >
+          DELETE
+        </MotionButton>
+      </Flex>
     </>
   );
 }
@@ -22,7 +53,7 @@ function DeleteProductModal({ closeModal }: ModalProps) {
     >
       <Modal
         handleClose={closeModal}
-        className="h-[30rem] w-[50rem] bg-white p-8 mx-8 rounded-lg flex flex-col items-center"
+        className="relative h-[12rem] w-[24rem] bg-white p-8 rounded-lg flex flex-col items-center justify-center"
       >
         <DeleteProductContent closeModal={closeModal} />
       </Modal>
