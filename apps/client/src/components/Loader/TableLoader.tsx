c8e700a8143321e8cb906728916adf67a753bfd0
RedDotz20 <carlos_tabangay@outlook.com>
2023-04-08 22:28:57 +0800

    chore: table loader

--- apps/client/src/components/Loader/TableLoader.tsx
+++ apps/client/src/components/Loader/TableLoader.tsx
@@ -1,42 +1,18 @@
-import { Spinner, Th, Thead, Tr, Flex, Heading } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
-import { fadeInOut } from './variants';
+import { Skeleton, Tbody, Td, Tr } from '@chakra-ui/react';
 
 function TableLoader() {
-  const MotionFlex = motion(Flex);
   return (
-    <Thead>
+    <Tbody>
       <Tr>
-        <Th
-          colSpan={8}
-          width="100%"
-          textAlign="center"
-          verticalAlign="middle"
-          height="100px"
-        >
-          <MotionFlex
-            justify="center"
-            align="center"
-            initial={{ opacity: 0 }}
-            variants={fadeInOut}
-            animate="animate"
-            exit="exit"
-          >
-            <Spinner
-              thickness="3.5px"
-              speed="0.6s"
-              emptyColor="gray.200"
-              color="orange"
-              size="md"
-              mr={4}
-            />
-            <Heading as="h2" size="md">
-              Loading ...
-            </Heading>
-          </MotionFlex>
-        </Th>
+        <Td colSpan={8}>
+          <Skeleton height="4rem" my={3} />
+          <Skeleton height="4rem" my={3} />
+          <Skeleton height="4rem" my={3} />
+          <Skeleton height="4rem" my={3} />
+          <Skeleton height="4rem" my={3} />
+        </Td>
       </Tr>
-    </Thead>
+    </Tbody>
   );
 }
 
