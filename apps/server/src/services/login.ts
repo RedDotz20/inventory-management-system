7b848f99f0c4a1dffd98ad8c86888d48e35ec5a2
RedDotz20 <carlos_tabangay@outlook.com>
2023-04-19 21:34:02 +0800

    update: generate login access | refresh token

---
+++ apps/server/src/services/login.ts
@@ -0,0 +1,64 @@
+import bcryptjs from 'bcryptjs';
+import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import config from '../config';
+import connection from '../config/connection';
+
+const JWT_SECRET = config.server.token.secret;
+
+async function login(req: Request, res: Response) {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
+
+    const { username, password } = req.body;
+    const query = `SELECT * FROM users WHERE username=?`;
+
+    await connection.execute(query, [username], async (error, user) => {
+      if (error) throw error;
+      if (user.length === 0) {
+        console.log('Invalid username or password');
+        return res.status(404).send('Invalid username or password');
+      }
+
+      const isMatched = await bcryptjs.compare(password, user[0].password);
+
+      if (!isMatched) {
+        console.log('Invalid username or password');
+        return res
+          .status(404)
+          .json({ message: 'Invalid Username or Password' });
+      }
+
+      //? Generate Access Token
+      const accessToken = jwt.sign(
+        { id: user[0].id, username: user[0].username },
+        JWT_SECRET,
+        { expiresIn: '10m' }
+      );
+
+      //? Generate Refresh Token
+      const refreshToken = jwt.sign({ id: user[0].id }, JWT_SECRET, {
+        expiresIn: '7d'
+      });
+
+      //? Store Refresh Token inside of Cookie as httpOnly
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7 //* 7 days
+      });
+
+      //? Send Access Token as Response to store in Local storage
+      res.status(200).json({ accessToken, redirectURL: '/home/dashboard' });
+      console.log('User Authenticated Sucessfully');
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Server error' });
+  }
+}
+
+export default { login };
