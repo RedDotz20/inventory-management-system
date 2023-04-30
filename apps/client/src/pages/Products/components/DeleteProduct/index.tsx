282996a1a19c0aa1e1ce2b0bbf96e45935269198
RedDotz20 <carlos_tabangay@outlook.com>
2023-04-30 17:58:17 +0800

    perf: delete product load

--- apps/client/src/pages/Products/components/DeleteProduct/index.tsx
+++ apps/client/src/pages/Products/components/DeleteProduct/index.tsx
@@ -1,31 +1,43 @@
 import { DeleteIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { lazy } from 'react';
+import { Suspense, lazy, useState } from 'react';
 import useDeleteProductStore from './DeleteProductStore';
 
 const DeleteProductModal = lazy(() => import('./DeleteProductModal'));
 
 function DeleteProduct({ productId }: { productId: number }) {
+  const [isloading, setIsLoading] = useState(false);
   const { deleteIsOpen, openDeleteModal, closeDeleteModal } =
     useDeleteProductStore();
   const MotionButton = motion(Button);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    await import('./DeleteProductModal');
+    setIsLoading(false);
+    openDeleteModal();
+  };
+
   return (
     <>
       <AnimatePresence initial={false} mode="wait" onExitComplete={() => null}>
         {deleteIsOpen && (
-          <DeleteProductModal
-            productId={productId}
-            closeModal={closeDeleteModal}
-          />
+          <Suspense>
+            <DeleteProductModal
+              productId={productId}
+              closeModal={closeDeleteModal}
+            />
+          </Suspense>
         )}
       </AnimatePresence>
 
       <MotionButton
-        onClick={() => openDeleteModal()}
+        onClick={() => handleClick()}
         whileTap={{ scale: 0.9 }}
         colorScheme="red"
         borderRadius="full"
+        isLoading={isloading}
         p="0"
       >
         <DeleteIcon />
