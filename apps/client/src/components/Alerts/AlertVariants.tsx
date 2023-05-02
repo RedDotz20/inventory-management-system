a9399929afbf97abf1d91bef9890691768346593
RedDotz20 <carlos_tabangay@outlook.com>
2023-05-02 14:17:09 +0800

    fix: alert params

--- apps/client/src/components/Alerts/AlertVariants.tsx
+++ apps/client/src/components/Alerts/AlertVariants.tsx
@@ -1,38 +1,18 @@
-import { toast } from 'react-toastify';
+import { toast, ToastOptions } from 'react-toastify';
 
-export const successAlert = (success: string) =>
-  toast.success('Login Successful', {
-    position: 'top-right',
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'colored'
-  });
-
-export const errorAlert = (error: string) =>
-  toast.error('Invalid Username or Password', {
-    position: 'top-right',
-    autoClose: 4000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'colored'
-  });
-
-export const infoAlert = (info: string) => {
-  toast.info('Logout Successful', {
-    position: 'top-right',
-    autoClose: 4000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'colored'
-  });
+const options: ToastOptions = {
+  position: 'top-right',
+  autoClose: 4000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored'
 };
+
+const successAlert = (success: string) => toast.success(success, options);
+const errorAlert = (error: string) => toast.error(error, options);
+const infoAlert = (info: string) => toast.info(info, options);
+
+export { successAlert, errorAlert, infoAlert };
