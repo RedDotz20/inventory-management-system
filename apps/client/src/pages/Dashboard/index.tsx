60e16fb27cda4e05b1b5eff7c590b81b517ed735
RedDotz20 <carlos_tabangay@outlook.com>
2023-05-18 22:37:27 +0800

    refactor: dashboard modify

--- apps/client/src/pages/Dashboard/index.tsx
+++ apps/client/src/pages/Dashboard/index.tsx
@@ -1,9 +1,23 @@
-import { Flex, Heading, Stack, Text } from '@chakra-ui/react';
-import Card from '../../components/Card';
+import {
+  Divider,
+  Flex,
+  Heading,
+  Icon,
+  Stack,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text
+} from '@chakra-ui/react';
+import { BsFillBarChartFill, BsFillBoxFill } from 'react-icons/bs';
+import { FaWrench } from 'react-icons/fa';
+import { HiDocumentText, HiTemplate } from 'react-icons/hi';
 
 export default function Dashboard() {
   return (
-    <Flex width="full" direction="column" p={4}>
+    <Flex direction="column" p={4}>
       <Stack py={4} mb={6} spacing={0}>
         <Text size="sm" color="white">
           home/dashboard
@@ -13,19 +27,224 @@ export default function Dashboard() {
         </Heading>
       </Stack>
 
-      <Flex width="full" gap={4}>
-        <Card
-          header="Total Products"
-          body={'10'}
-          color="text-blue-500"
-          borderColor="border-black"
-        />
-        <Card
-          header="Total Sales"
-          body={'100'}
-          color="text-green-500"
-          borderColor="border-black"
-        />
+      <Heading as="h1" size="lg" mt={0} mb={4}>
+        Sales Status
+      </Heading>
+
+      <Flex wrap="wrap">
+        <Flex gap={4} mb={4}>
+          <Stat
+            border="4px solid"
+            borderColor="border-black"
+            borderRadius={8}
+            p={2}
+            maxWidth="250px"
+            minWidth="250px"
+            display="flex"
+            alignItems="center"
+          >
+            <Flex alignItems="center">
+              <Icon as={BsFillBarChartFill} boxSize={12} mx={6} />
+              <Flex flexDirection="column">
+                <StatLabel>Today Sales</StatLabel>
+                <StatNumber>₱72,350</StatNumber>
+                <StatHelpText>
+                  <StatArrow type="decrease" />
+                  23.36%
+                </StatHelpText>
+              </Flex>
+            </Flex>
+          </Stat>
+
+          <Stat
+            border="4px solid"
+            borderColor="border-black"
+            borderRadius={8}
+            p={2}
+            maxWidth="250px"
+            minWidth="250px"
+            display="flex"
+            alignItems="center"
+          >
+            <Flex alignItems="center">
+              <Icon as={BsFillBarChartFill} boxSize={12} mx={6} />
+              <Flex flexDirection="column">
+                <StatLabel>Weekly Sales</StatLabel>
+                <StatNumber>₱123,516</StatNumber>
+                <StatHelpText>
+                  <StatArrow type="increase" />
+                  14.05%
+                </StatHelpText>
+              </Flex>
+            </Flex>
+          </Stat>
+
+          <Stat
+            border="4px solid"
+            borderColor="border-black"
+            borderRadius={8}
+            p={2}
+            maxWidth="250px"
+            minWidth="250px"
+            display="flex"
+            alignItems="center"
+          >
+            <Flex alignItems="center">
+              <Icon as={BsFillBarChartFill} boxSize={12} mx={6} />
+              <Flex flexDirection="column">
+                <StatLabel>Monthly Sales</StatLabel>
+                <StatNumber>₱376,540</StatNumber>
+                <StatHelpText>
+                  <StatArrow type="increase" />
+                  8.12%
+                </StatHelpText>
+              </Flex>
+            </Flex>
+          </Stat>
+        </Flex>
+      </Flex>
+
+      <Divider borderWidth="2px" borderColor="black" mb={2} maxWidth="780px" />
+
+      <Heading as="h1" size="lg" mt={0} mb={4}>
+        Inventory Status
+      </Heading>
+
+      <Flex>
+        <Flex maxWidth="1028px" gap={4} wrap="wrap">
+          <Stat
+            border="4px solid"
+            borderColor="border-black"
+            borderRadius={8}
+            p={2}
+            minWidth="250px"
+            maxWidth="250px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Flex alignItems="center">
+              <Icon as={FaWrench} boxSize={10} mr={4} ml={0} />
+              <Flex flexDirection="column">
+                <StatLabel fontSize="lg" fontWeight={600}>
+                  Registered Products
+                </StatLabel>
+                <StatNumber>28</StatNumber>
+              </Flex>
+            </Flex>
+          </Stat>
+
+          <Stat
+            border="4px solid"
+            borderColor="border-black"
+            borderRadius={8}
+            p={2}
+            minWidth="250px"
+            maxWidth="250px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Flex alignItems="center">
+              <Icon as={HiTemplate} boxSize={12} mr={4} ml={0} />
+              <Flex flexDirection="column">
+                <StatLabel fontSize="lg" fontWeight={600}>
+                  Registered items
+                </StatLabel>
+                <StatNumber>250</StatNumber>
+              </Flex>
+            </Flex>
+          </Stat>
+
+          <Stat
+            border="4px solid"
+            borderColor="border-black"
+            borderRadius={8}
+            p={2}
+            minWidth="250px"
+            maxWidth="250px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Flex alignItems="center">
+              <Icon as={HiDocumentText} boxSize={12} mr={4} ml={0} />
+              <Flex flexDirection="column">
+                <StatLabel fontSize="lg" fontWeight={600}>
+                  Purchased Orders
+                </StatLabel>
+                <StatNumber>54</StatNumber>
+              </Flex>
+            </Flex>
+          </Stat>
+
+          <Flex gap={4} flexWrap="wrap">
+            <Stat
+              border="4px solid"
+              borderColor="border-black"
+              borderRadius={8}
+              p={2}
+              minWidth="250px"
+              maxWidth="250px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Flex alignItems="center">
+                <Icon as={BsFillBoxFill} boxSize={12} mx={6} />
+                <Flex flexDirection="column">
+                  <StatLabel fontSize="lg" fontWeight={600}>
+                    Available In Stock Items
+                  </StatLabel>
+                  <StatNumber>232</StatNumber>
+                </Flex>
+              </Flex>
+            </Stat>
+
+            <Stat
+              border="4px solid"
+              borderColor="border-black"
+              borderRadius={8}
+              p={2}
+              minWidth="250px"
+              maxWidth="250px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Flex alignItems="center">
+                <Icon as={BsFillBoxFill} boxSize={12} mx={6} />
+                <Flex flexDirection="column">
+                  <StatLabel fontSize="lg" fontWeight={600}>
+                    Low In Stock Items
+                  </StatLabel>
+                  <StatNumber>9</StatNumber>
+                </Flex>
+              </Flex>
+            </Stat>
+            <Stat
+              border="4px solid"
+              borderColor="border-black"
+              borderRadius={8}
+              p={2}
+              minWidth="250px"
+              maxWidth="250px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Flex alignItems="center">
+                <Icon as={BsFillBoxFill} boxSize={12} mx={6} />
+                <Flex flexDirection="column">
+                  <StatLabel fontSize="lg" fontWeight={600}>
+                    Out of Stock Items
+                  </StatLabel>
+                  <StatNumber>2</StatNumber>
+                </Flex>
+              </Flex>
+            </Stat>
+          </Flex>
+        </Flex>
       </Flex>
     </Flex>
   );
--- apps/client/src/pages/Home/index.tsx
+++ apps/client/src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import SidebarRoutes from './SidebarRoutes';
 
 function Home() {
   return (
-    <div className="relative flex h-screen">
+    <div className="relative flex h-screen ">
       <div className="bg-[#F77E21] w-full h-28 absolute -z-50"></div>
       <Sidebar />
       <SidebarRoutes />
